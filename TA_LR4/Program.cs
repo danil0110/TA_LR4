ff9bfc4b5c3e41caa33c266f3ff32ceb3a6c289a
danil0110 <danilmail0110@gmail.com>
2020-05-25 15:20:49 +0300

    Initial commit

---
+++ TA_LR4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TA_LR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
